d78cdd8cac138bdf95e512acd652da1cea9065c6
myang1010 <myang1010@gmail.com>
2021-06-22 15:48:37 -0700

    submit week13 assignments

--- app/assets/javascripts/ajax_ops.js
+++ app/assets/javascripts/ajax_ops.js
@@ -1,96 +1,175 @@
 function handle_ajax(event) {
-  console.log('DOM fully loaded and parsed');
-  const resultsDiv = document.getElementById('results-div');
-  const restOpsDiv = document.getElementById('rest-ops');
-  const listUsersButton = document.getElementById('list-users');
-  const createUserButton = document.getElementById('create-user');
-  const userName = document.getElementById('user-username');
-  const userPassword = document.getElementById('user-password');
-  const updateUserButton = document.getElementById('update-user')
-  const userID = document.getElementById('user-id')
-  const userName1 = document.getElementById('user-username1')
-  const userPassword1 = document.getElementById('user-password1')
-  const users_path = 'http://localhost:3001/api/v1/users'
+    console.log('DOM fully loaded and parsed');
+    const resultsDiv = document.getElementById('results-div');
+    const restOpsDiv = document.getElementById('rest-ops');
+  
+    const listUsersButton = document.getElementById('list-users');
+  
+    const userName = document.getElementById('user-username');
+    const userPassword = document.getElementById('user-password');
+    const createUserButton = document.getElementById('create-user');
+  
+    const userID = document.getElementById('user-id')
+    const userName1 = document.getElementById('user-username1')
+    const userPassword1 = document.getElementById('user-password1')
+    const updateUserButton = document.getElementById('update-user')
+  
+    const userID1 = document.getElementById('user-id1')
+    const deleteUserButton = document.getElementById('delete-user')
+    
+    const userID2 = document.getElementById('user-id2')
+    const listFactsButton = document.getElementById('list-facts')
+  
+    const userID3 = document.getElementById('user-id3')
+    const factInput = document.getElementById('fact_text')
+    const likesInput = document.getElementById('likes')
+    const addFactButton = document.getElementById('add-fact')
+  
+    const userID4 = document.getElementById('user-id4')
+    const factID = document.getElementById('fact-id')
+    const factInput1 = document.getElementById('fact_text1')
+    const likesInput1 = document.getElementById('likes1')
+    const updateFactButton = document.getElementById('update-fact')
+    
+    const userID5 = document.getElementById('user-id5')
+    const factID1 = document.getElementById('fact-id1')
+    const deleteFactButton = document.getElementById('delete-fact')
+  
+    const users_path = 'http://localhost:3001/api/v1/users'
 
-  restOpsDiv.addEventListener('click', (event) => {
-    if (event.target === listUsersButton) {
-      fetch(users_path).then((response) => {
-        if (response.status === 200) {
-          resultsDiv.innerHTML = '';
+    function getResponse(response){ 
+      if (response.status === 200 || 201) {
           response.json().then((data) => {
-            for (let i=0; i<data.length; i++) {
-              let parag = document.createElement('P');
-              parag.textContent = JSON.stringify(data[i]);
-              resultsDiv.appendChild(parag);
+            resultsDiv.innerHTML = '';
+            let parag = document.createElement('P');
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+      } else {
+          response.json().then((data) => {
+            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
+          }).catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+      }
+    }  
+  
+    restOpsDiv.addEventListener('click', (event) => {
+      if (event.target === listUsersButton) {
+        fetch(users_path).then((response) => {
+          if (response.status === 200) {
+            resultsDiv.innerHTML = '';
+            response.json().then((data) => {
+              for (let i=0; i<data.length; i++) {
+                let parag = document.createElement('P');
+                parag.textContent = JSON.stringify(data[i]);
+                resultsDiv.appendChild(parag);
+              }
+            });
+          } else {
+            alert(`Return code ${response.status} ${response.statusText}`);
+          }
+        }).catch((error) => {
+          console.log(error);
+          alert(error);
+        });
+      } else if (event.target === createUserButton) {
+        var dataObject = {
+          username: userName.value,
+          password: userPassword.value
+        }
+        fetch(users_path,
+          { method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dataObject)
+          }
+        ).then(getResponse);
+      } else if (event.target === updateUserButton) {
+        var dataObject = {
+          username: userName1.value,
+          password: userPassword1.value
+        }
+        if (dataObject.username === "") {  // blank usernames not supported
+          delete dataObject.username;
+        }
+        if (dataObject.password === "") { // blank passwords not supported
+          delete dataObject.password;
+        }
+        fetch(`${users_path}/${userID.value}`,
+          { method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dataObject)
+          }
+        ).then(getResponse);
+      } else if (event.target === deleteUserButton) {
+        fetch(`${users_path}/${userID1.value}`,
+          { method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+          }
+        ).then(getResponse);
+      } else if (event.target === listFactsButton) {
+        fetch(`${users_path}/${userID2.value}/facts`,
+          { method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dataObject)
+          }
+          ). then((response) => { 
+              if (response.status === 200) {
+                resultsDiv.innerHTML = '';
+                response.json().then((data) => {
+                for (let i=0; i<data.length; i++) {
+                    let parag = document.createElement('P');
+                    parag.textContent = JSON.stringify(data[i]);
+                    resultsDiv.appendChild(parag);
+                }
+                });
+              } else {
+                    alert(`Return code ${response.status} ${response.statusText}`);
+              }
+          }).catch((error) => {
+            console.log(error);
+            alert(error);
+            });
+      } else if (event.target === addFactButton) {
+        var dataObject = {
+          fact_text: factInput.value,
+          likes: likesInput.value,
+          user_id: `${userID3.value}`
+        }
+        fetch(`${users_path}/${userID3.value}/facts`,
+          { method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dataObject)
+          }
+        ).then(getResponse);
+      } else if (event.target === updateFactButton) {
+        var dataObject = {
+          fact_text: factInput1.value,
+          likes: likesInput1.value
+        }
+        if (dataObject.fact_text === "") {  // blank usernames not supported
+          delete dataObject.fact_text;
+        }
+        if (dataObject.likes === "") { // blank passwords not supported
+          delete dataObject.likes;
+        }
+        fetch(`${users_path}/${userID4.value}/facts/${factID.value}`,
+          { method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dataObject)
+          }
+        ).then(getResponse);
+      } else if (event.target === deleteFactButton) {
+        if (userID5.value != "" && factID1.value != ""){
+            fetch(`${users_path}/${userID5.value}/facts/${factID1.value}`,
+            { method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
             }
-          });
-        } else {
-          alert(`Return code ${response.status} ${response.statusText}`);
-        }
-      }).catch((error) => {
-        console.log(error);
-        alert(error);
-      });
-    } else if (event.target === createUserButton) {
-      var dataObject = {
-        username: userName.value,
-        password: userPassword.value
-      }
-      fetch(users_path,
-        { method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(dataObject)
-        }
-      ).then((response) => {
-        if (response.status === 201) {
-          response.json().then((data) => {
-            resultsDiv.innerHTML = '';
-            let parag = document.createElement('P');
-            parag.textContent = JSON.stringify(data);
-            resultsDiv.appendChild(parag);
-          });
-        } else {
-          response.json().then((data) => {
-            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
-          }).catch((error) => {
-            console.log(error);
-            alert(error);
-          });
-        }
-      });
-    } else if (event.target === updateUserButton) {
-      var dataObject = {
-        username: userName1.value,
-        password: userPassword1.value
-      }
-      if (dataObject.username === "") {  // blank usernames not supported
-        delete dataObject.username;
-      }
-      if (dataObject.password === "") { // blank passwords not supported
-        delete dataObject.password;
-      }
-      fetch(`${users_path}/${userID.value}`,
-        { method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(dataObject)
-        }
-      ).then((response) => {
-        if (response.status === 200) {
-          response.json().then((data) => {
-            resultsDiv.innerHTML = '';
-            let parag = document.createElement('P');
-            parag.textContent = JSON.stringify(data);
-            resultsDiv.appendChild(parag);
-          });
-        } else {
-          response.json().then((data) => {
-            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
-          }).catch((error) => {
-            console.log(error);
-            alert(error);
-          });
-        }
-      });
-    }
-  });
-}
+            ).then(getResponse);
+        } else{
+            alert(`Both user_id and fact_id are required.`);
+          }    
+      } 
+    });
+  }
